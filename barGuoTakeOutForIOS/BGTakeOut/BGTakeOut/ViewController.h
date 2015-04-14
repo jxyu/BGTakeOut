b999e016311628e85c7e98f1f12bc1a4d8b8527c
于金祥 <1028920761@qq.com>
2015-04-14 09:24:32 +0800

    巴国外卖新建项目

---
+++ barGuoTakeOutForIOS/BGTakeOut/BGTakeOut/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BGTakeOut
+//
+//  Created by 于金祥 on 15/4/14.
+//  Copyright (c) 2015年 jxyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ barGuoTakeOutForIOS/BGTakeOut/BGTakeOut/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BGTakeOut
+//
+//  Created by 于金祥 on 15/4/14.
+//  Copyright (c) 2015年 jxyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
