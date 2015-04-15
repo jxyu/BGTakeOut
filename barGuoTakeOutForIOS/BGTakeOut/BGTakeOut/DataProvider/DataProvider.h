88dcb4647423ea1a98887a32c741270adb2fa907
于金祥 <1028920761@qq.com>
2015-04-15 14:37:42 +0800

    no message

---
+++ barGuoTakeOutForIOS/BGTakeOut/BGTakeOut/DataProvider/DataProvider.h
@@ -0,0 +1,13 @@
+//
+//  DataProvider.h
+//  BGTakeOut
+//
+//  Created by 于金祥 on 15/4/15.
+//  Copyright (c) 2015年 jxyu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DataProvider : NSObject
+
+@end
---
+++ barGuoTakeOutForIOS/BGTakeOut/BGTakeOut/DataProvider/DataProvider.m
@@ -0,0 +1,13 @@
+//
+//  DataProvider.m
+//  BGTakeOut
+//
+//  Created by 于金祥 on 15/4/15.
+//  Copyright (c) 2015年 jxyu. All rights reserved.
+//
+
+#import "DataProvider.h"
+
+@implementation DataProvider
+
+@end
