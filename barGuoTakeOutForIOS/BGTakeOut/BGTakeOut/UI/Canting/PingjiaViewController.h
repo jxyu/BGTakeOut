cfdb4be9f2dd762c27de22db76edb4cf51566869
粒橙Leo <yanglicheng8@126.com>
2015-05-16 18:09:38 +0800

    修改相关功能，添加个人中心的金币商城

---
+++ barGuoTakeOutForIOS/BGTakeOut/BGTakeOut/UI/Canting/PingjiaViewController.h
@@ -0,0 +1,13 @@
+//
+//  PingjiaViewController.h
+//  BGTakeOut
+//
+//  Created by 粒橙Leo on 15/5/16.
+//  Copyright (c) 2015年 jxyu. All rights reserved.
+//
+
+#import "BaseNavigationController.h"
+
+@interface PingjiaViewController : BaseNavigationController
+
+@end
