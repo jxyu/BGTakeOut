af8186c908918445316eeb812fe90e8cee4ef2a6
粒橙Leo <yanglicheng8@126.com>
2015-05-20 12:16:52 +0800

    更新商铺订单评价界面

--- barGuoTakeOutForIOS/BGTakeOut/BGTakeOut/UI/Canting/CantingInfoViewController.m
+++ barGuoTakeOutForIOS/BGTakeOut/BGTakeOut/UI/Canting/CantingInfoViewController.m
@@ -713,6 +713,7 @@
     [dataprovider GetPinglun:_resid andpage:@"1" andnumInPage:@"6" andiscontaintext:@"1"];
     
     PingjiaViewController * pingjia =[[PingjiaViewController alloc] init];
+    pingjia.resid=_resid;
     [self.navigationController pushViewController:pingjia animated:YES];
     
 }
