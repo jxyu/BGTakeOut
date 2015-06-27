e143e1e51a64b430e69c9ac1c0afa5d07f020acc
于金祥 <1028920761@qq.com>
2015-06-27 18:43:32 +0800

    修改完成

---
+++ barGuoTakeOutForIOS/BGTakeOut/BGTakeOut/UI/BGB/AreaSelectViewController.h
@@ -0,0 +1,13 @@
+//
+//  AreaSelectViewController.h
+//  BGTakeOut
+//
+//  Created by 于金祥 on 15/6/27.
+//  Copyright (c) 2015年 jxyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AreaSelectViewController : UIViewController
+
+@end
---
+++ barGuoTakeOutForIOS/BGTakeOut/BGTakeOut/UI/BGB/AreaSelectViewController.m
@@ -0,0 +1,37 @@
+//
+//  AreaSelectViewController.m
+//  BGTakeOut
+//
+//  Created by 于金祥 on 15/6/27.
+//  Copyright (c) 2015年 jxyu. All rights reserved.
+//
+
+#import "AreaSelectViewController.h"
+
+@interface AreaSelectViewController ()
+
+@end
+
+@implementation AreaSelectViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
